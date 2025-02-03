8d5835a378ad7577866b0c309b5afd0bcefa3ead
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-03 19:59:33 +0100

    add Box and SuperButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+// @ts-ignore
+// @ts-ignore
+
 import './App.css'
 import styled from 'styled-components';
 
@@ -5,7 +8,10 @@ function App() {
 
   return (
       <div className={"App"}>
-          <StyledBtn>Hello</StyledBtn>
+          <Box>
+              <StyledBtn>Hello</StyledBtn>
+              <SuperButton>Super Button</SuperButton>
+          </Box>
       </div>
   )
 }
@@ -16,8 +22,20 @@ const StyledBtn = styled.button`
     border: none;
     background-color: #c023a4;
     padding: 10px 20px;
-    border-radius: 5px;
     color: snow;
     font-size: 2rem;
     font-weight: bold;
+`
+
+const SuperButton = styled(StyledBtn)`
+    border-radius: 5px;
+    background-color: #e1fd01;
+    color: #898787;
+`
+
+const Box = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `
