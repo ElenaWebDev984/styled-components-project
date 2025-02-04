afa1deac180466d3838188426378f97717326473
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-04 03:03:13 +0100

    add animation

--- src/components/Button.styled.tsx
+++ src/components/Button.styled.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {MyAnimation} from '../styles/animations/Animations.tsx';
 
 export const StyledBtn = styled.button`
     border: none;
@@ -22,4 +23,8 @@ export const SuperButton = styled(StyledBtn)`
     border-radius: 5px;
     background-color: #e1fd01;
     color: #898787;
+    
+    &:hover {
+        animation: ${MyAnimation} 2s ease-in-out infinite;  
+    }
 `
