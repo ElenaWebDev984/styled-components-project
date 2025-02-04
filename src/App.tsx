cd1c8b0709bf76614a57c96da347d700e8533315
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-02-04 02:31:00 +0100

    add Menu

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,14 @@ function App() {
 
   return (
       <div className={"App"}>
+          <Menu>
+              <ul>
+                  <li><a href="">item 1</a></li>
+                  <li><a href="">item 2</a></li>
+                  <li><a href="">item 3</a></li>
+              </ul>
+          </Menu>
+
           <Box>
               <StyledBtn as={Link} href={"#"}>LinkComponent</StyledBtn>
               <StyledBtn as={"a"} href={"#"}>Link</StyledBtn>
@@ -27,12 +35,22 @@ const StyledBtn = styled.button`
     color: snow;
     font-size: 2rem;
     font-weight: bold;
+
+    &:hover {
+        background-color: #0f1cef;
+    }
+
+    &:last-child {
+        background-color: #10c310;
+    }
 `
 
 const Link = styled.a`
     color: #747bff;
     font-size: 2rem;
     font-weight: bold;
+    background-color: transparent;
+    padding: 0;
 `
 
 const SuperButton = styled(StyledBtn)`
@@ -46,6 +64,34 @@ const Box = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: 20px;
+    
+    button {
+        cursor: pointer;
+    }
+
+${Link} {
+    cursor: zoom-in;
+}
+`
+
+
+const Menu = styled.nav`
+    ul {
+        list-style: none;
+        padding: 0;
+        display: flex;
+        
+        //комбинаторы
+        li > a {
+            color: green;
+        }
+        
+        // братский селектор(li, который идет перед li)
+        li + li {
+            margin-left: 20px;
+        }
+    }
 `
 
 
